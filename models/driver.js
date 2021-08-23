a7785439fa20f1589bab08f4044fa132e0a2d39b
qasem-moh <2171246@s.aou.edu.jo>
2021-08-23 09:26:56 +0300

    lab11"

--- models/driver.js
+++ models/driver.js
@@ -3,18 +3,18 @@
 const events = require('../events');
 
 events.on('driverPickup', payload => {
-    setTimeout(() => {
-        console.log(`DRIVER: picked up ${payload.orderID}`);
-        events.emit('transit', payload)
-    }, 1000)
+  setTimeout(() => {
+    console.log(`DRIVER:${payload.orderID}`);
+    events.emit('transit', payload);
+  }, 1000);
 });
 
 
 events.on('drivertransit', payload => {
-    setTimeout(() => {
-        console.log(`DRIVER: delivered up ${payload.orderID}`);
-        events.emit('driverDileverd', payload)
-    }, 3000);
-})
+  setTimeout(() => {
+    console.log(`DRIVER: ${payload.orderID}`);
+    events.emit('driverDileverd', payload);
+  }, 3000);
+});
 
-module.exports = events
+module.exports = events;
